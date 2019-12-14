18255bc9c39b41c578f643cb42ec8c5a74899d0d
3263927 <3263927@gmail.com>
2019-12-15 03:09:36 +0400

    3 Connections

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -12,6 +12,7 @@ using WebApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace WebApp
 {
@@ -29,7 +30,7 @@ namespace WebApp
 		{
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(
-					Configuration.GetConnectionString("DefaultConnection")));
+					Configuration.GetConnectionString("Habr1_Local")));
 			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddControllersWithViews();
